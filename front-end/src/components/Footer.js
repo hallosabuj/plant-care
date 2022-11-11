8cde9549aa3e8231b68ba45d1666a58f2ad6ec1b
Sabuj Mondal <hallosabuj@gmail.com>
2022-11-11 23:05:30 +0530

    Plant Details UI added

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -7,21 +7,21 @@ class Footer extends Component {
       //   <h1 className='text-red-200'>This is footer neen to add valid values</h1>
       // </div>
 
-      <footer class="p-4 bg-lime-300 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="#" class="hover:underline">Plant Care™</a>. All Rights Reserved.
+      <footer className="p-4 bg-lime-300 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="#" className="hover:underline">Plant Care™</a>. All Rights Reserved.
         </span>
-        <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+        <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+            <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
           </li>
           <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+            <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
           </li>
           <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
+            <a href="#" className="mr-4 hover:underline md:mr-6">Licensing</a>
           </li>
           <li>
-            <a href="#" class="hover:underline">Contact</a>
+            <a href="#" className="hover:underline">Contact</a>
           </li>
         </ul>
       </footer>
--- front-end/src/components/Plants/PlantDetails.js
+++ front-end/src/components/Plants/PlantDetails.js
@@ -1,18 +1,45 @@
+import axios from 'axios';
 import React, { Component } from 'react'
 
 class PlantDetails extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      plantDetails: null
+    }
+  }
+  async getDetails(){
+    let plantId = window.location.href.split('/')[4]
+    await axios.get("/plant/" + plantId).then((response) => {
+      console.log(response.data)
+      this.setState({
+        plantDetails: response.data
+      })
+    }).catch(function (error) {
+      console.log(error);
+    });
+  }
+  componentDidMount() {
+    this.getDetails()  
+  }
   render() {
-    let plantId=window.location.href.split('/')[4]
-    return (
+    let plantId = window.location.href.split('/')[4]
+    return !(this.state.plantDetails) ? (<div>Details Not Found</div>) : (
       <div>
         <div className='grid grid-cols-3 gap-2 p-2'>
           {/* Row 1 */}
-          <div className=' col-span-3 bg-slate-500 h-14'><h1>PlantDetails: {plantId}</h1></div>
+          <div className=' col-span-3 bg-slate-500 h-14'><h1>{this.state.plantDetails.name}</h1></div>
           {/* Row 2 */}
           <div className=' bg-slate-400'>Applied On 12/12/22</div>
-          <div className=' col-span-2 row-span-3 bg-slate-400'><img alt="plant Image" style={{height:"200px"}}/></div>
+          <div className=' col-span-2 row-span-3 bg-slate-400 flex float-left overflow-hidden'>
+            {this.state.plantDetails.imageNames.map((imageName)=>{
+              let imageUrl="http://localhost:8080/plant/downloadImage/" + imageName;
+              return (<img alt="plant Image" key={imageName} src={imageUrl} style={{ height: "200px" }} className="p-1"/>)
+            })}
+          </div>
           {/* Row 3 */}
-          <div className=' bg-slate-500'> DOB: 11/11/22</div>
+          <div className=' bg-slate-500'> DOB: {this.state.plantDetails.dob}</div>
           {/* Row 4 */}
           <div className=' bg-slate-400'>Apply Interval: 10</div>
           {/* Row 5 */}
