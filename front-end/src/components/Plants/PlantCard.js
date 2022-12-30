f8c465836ce53a7df555501ef0c672e42bc564e0
Sabuj Mondal <hallosauj@gmail.com>
2022-12-30 23:48:34 +0530

    Selected tab highlighting in NavBar

--- front-end/src/components/Plants/PlantCard.js
+++ front-end/src/components/Plants/PlantCard.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import Card from '@mui/material/Card';
 import deleteIcon from '../../delete.png';
 import waterIcon from '../../water.png';
 import axios from 'axios';
@@ -24,12 +23,12 @@ class PlantCard extends Component {
     let imageUrl = "/api/plant/downloadImage/" + this.state.plant.profileImage
     return (
       <div className='pr-1 pl-1 pt-3 pb-3 shadow-lg hover:scale-105'>
-        <Card sx={{ border: 1, borderRadius: 2, height: "405px", backgroundColor: "cyan" }}>
-          <div className='h-[300px] bg-lime-900 flex justify-center items-center p-2 rounded-md relative' >
+        <div className='h-[420px] bg-cyan-400'>
+          <div className='h-[320px] bg-lime-900 flex justify-center items-center p-2 rounded-md relative' >
             <Link to={"/web/plants/" + this.state.plant.plantId}>
-              <img src={imageUrl} style={{ maxHeight: "100%", maxWidth: "100%" }} alt={this.state.plant.name} />
+              <img src={imageUrl} className=' max-h-1 max-w-1' alt={this.state.plant.name} />
             </Link>
-            <img src={deleteIcon} onClick={() => this.deletePlant()} alt="delete" className='hover:opacity-100 opacity-60 top-3 right-3 absolute' />
+            <img src={deleteIcon} onClick={() => this.deletePlant()} alt="delete" className='hover:opacity-100 opacity-60 top-3 right-3 absolute'/>
           </div>
           <div className=' bg-orange-200 h-[100px] rounded-md flex justify-center items-center'>
             <div className='grid grid-cols-2 grid-rows-2 gap-2 w-full p-2'>
@@ -37,14 +36,14 @@ class PlantCard extends Component {
               <div className=' flex p-2 items-center col-span-2 bg-slate-600 h-10'>{this.state.plant.name}</div>
               {/* Row 2 */}
               <div className='flex justify-center items-center bg-orange-400'>
-                <img src={waterIcon} className="h-6 w-6"></img>
+                <img src={waterIcon} className="h-6 w-6" alt={"Water"}></img>
               </div>
               <div className='flex justify-center items-center bg-orange-400'>
                 <div className='bg-red-700 h-4 w-4 rounded-1/2'></div>
               </div>
             </div>
           </div>
-        </Card>
+        </div>
       </div>
     );
   }
