55787d4a27126bdb8fd4b59ff6f0a64c26894d27
Sabuj Mondal <hallosauj@gmail.com>
2024-08-19 21:37:13 +0530

    Modification in the postion for few modals

--- front-end/src/components/fertilizers/Modals/AddFertilizerModal.js
+++ front-end/src/components/fertilizers/Modals/AddFertilizerModal.js
@@ -184,10 +184,8 @@ class AddFertilizerModal extends Component {
         </button>
         {showModal ? (
           <>
-            <div
-              className="justify-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-            >
-              <div className="relative w-auto my-6 mx-auto max-w-3xl">
+            <div className="justify-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+              <div className="relative w-auto my-6 mx-auto max-w-3xl top-16">
                 {/*content*/}
                 <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                   {/*header*/}
--- front-end/src/components/plants-user/Modals/Add_PlantModal.js
+++ front-end/src/components/plants-user/Modals/Add_PlantModal.js
@@ -190,10 +190,8 @@ class AddPlantModal extends Component {
         </button>
         {showModal ? (
           <>
-            <div
-              className="justify-center flex fixed inset-0 z-50 outline-none focus:outline-none bg-black bg-opacity-30 w-full backdrop-blur-sm"
-            >
-              <div className="relative w-auto my-6 mx-auto max-w-3xl">
+            <div className="justify-center flex fixed inset-0 z-50 outline-none focus:outline-none bg-black bg-opacity-30 w-full backdrop-blur-sm">
+              <div className="relative w-auto my-6 mx-auto max-w-3xl top-16">
                 {/*content*/}
                 <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                   {/*header*/}
