dc508d5cc9ee080550c23e795ab85ee52f189ce4
Sabuj Mondal <hallosauj@gmail.com>
2024-08-23 00:09:09 +0530

    Plant card UI modified

--- front-end/src/components/plants-common/PlantCard.js
+++ front-end/src/components/plants-common/PlantCard.js
@@ -14,24 +14,17 @@ class PlantCard extends Component {
     let imageUrl = "/api/plant/downloadImage/medium/" + this.state.plant.profileImage
     return (
       <div className='pr-1 pl-1 pt-3 pb-3 hover:scale-105 transition-all duration-300'>
-        <div className='lg:h-[420px] md:h-[420px] h-[300px] rounded-lg shadow-lg '>
-          <div className='lg:h-[320px] md:h-[320px] h-[200px] flex justify-center items-center rounded-lg relative overflow-hidden bg-slate-500 border-2 border-slate-900 p-3' >
+        <div className='lg:h-[370px] md:h-[370px] h-[250px] rounded-lg shadow-lg'>
+          <div className='lg:h-[320px] md:h-[320px] h-[200px] flex justify-center items-center rounded-t-lg relative overflow-hidden bg-slate-500 border-t-2 border-x-2 border-slate-900 p-3' >
             <Link to={"/web/plants/" + this.state.plant.plantId}>
-              <img src={imageUrl} className=' w-[100%] lg:h-[290px] md:h-[290px] h-[170px] object-cover rounded-md' alt={this.state.plant.name} />
+              <img src={imageUrl} className=' w-[100%] lg:h-[290px] md:h-[290px] h-[170px] object-cover rounded-md shadow-cyan-300 shadow-md' alt={this.state.plant.name} />
             </Link>
           </div>
-          <div className=' h-[100px] rounded-lg flex justify-center items-center bg-slate-500 border-2 border-slate-900'>
-            <div className='grid grid-cols-2 grid-rows-2 gap-2 w-full p-2 overflow-scroll scrollbar-hide'>
+          <div className=' h-[50px] rounded-b-lg flex justify-center items-center bg-slate-500 border-x-2 border-b-2 border-slate-900'>
+            <div className='grid gap-2 w-full p-2 overflow-scroll scrollbar-hide'>
               {/* Row 1 */}
-              <div className=' flex p-2 items-center col-span-2 text-stone-200 h-10'>
-                {this.state.plant.numberId}: {this.state.plant.name}
-              </div>
-              {/* Row 2 */}
-              <div className='flex justify-center items-center'>
-                <img src={waterIcon} className="h-6 w-6" alt={"Water"}></img>
-              </div>
-              <div className='flex justify-center items-center'>
-                <div className='bg-red-700 h-4 w-4 rounded-full'></div>
+              <div className=' flex p-2 items-center text-stone-200 h-10'>
+                {this.state.plant.name}
               </div>
             </div>
           </div>
--- front-end/src/components/plants-user/UserPlantCard.js
+++ front-end/src/components/plants-user/UserPlantCard.js
@@ -46,24 +46,22 @@ class UserPlantCard extends Component {
       <div>
         <div className='pr-1 pl-1 pt-3 pb-3 hover:scale-105 transition-all duration-300'>
           <div className='lg:h-[420px] md:h-[420px] h-[300px] rounded-lg shadow-lg '>
-            <div className='lg:h-[320px] md:h-[320px] h-[200px] flex justify-center items-center rounded-lg relative overflow-hidden bg-slate-500 border-2 border-slate-900 p-3' >
+            <div className='lg:h-[320px] md:h-[320px] h-[200px] flex justify-center items-center rounded-t-lg relative overflow-hidden bg-slate-500 border-x-2 border-t-2 border-slate-900 p-3' >
               <Link to={"/web/user/plants/" + this.state.plant.plantId}>
-                <img src={imageUrl} className='w-[100%] lg:h-[290px] md:h-[290px] h-[170px] object-cover rounded-md' alt={this.state.plant.name} />
+                <img src={imageUrl} className='w-[100%] lg:h-[290px] md:h-[290px] h-[170px] object-cover rounded-md shadow-cyan-300 shadow-md' alt={this.state.plant.name} />
               </Link>
               <img src={deleteIcon} onClick={this.confirmPlantDelete} alt="delete" className='hover:opacity-100 opacity-80 top-3 right-3 absolute h-7 w-7 md:h-9 md:w-9'/>
             </div>
-            <div className=' h-[100px] rounded-lg flex justify-center items-center bg-slate-500 border-2 border-slate-900'>
+            <div className=' h-[100px] rounded-b-lg flex justify-center items-center border-x-2 border-b-2 bg-slate-500 border-slate-900'>
               <div className='grid grid-cols-2 grid-rows-2 gap-2 w-full p-2 overflow-scroll scrollbar-hide'>
                 {/* Row 1 */}
-                <div className=' flex p-2 items-center col-span-2 text-stone-200 h-10'>
-                  {this.state.plant.numberId}: {this.state.plant.name}
+                <div className='flex p-2 items-center col-span-2 text-stone-200 h-10'>
+                  {this.state.plant.name}
                 </div>
                 {/* Row 2 */}
-                <div className='flex justify-center items-center'>
-                  <img src={waterIcon} className="h-6 w-6" alt={"Water"}></img>
-                </div>
-                <div className='flex justify-center items-center'>
-                  <div className='bg-red-700 h-4 w-4 rounded-full'></div>
+                <div className='flex p-2 items-center text-stone-200 h-10'>ID :</div>
+                <div className='flex justify-center items-center bg-red-300 hover:bg-red-500 shadow-black shadow-sm h-8 w-8 rounded-full'>
+                  <div>{this.state.plant.numberId}</div>
                 </div>
               </div>
             </div>
