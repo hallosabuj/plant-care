5b514e8a0d6b24706183f704012279e1cceab9e4
Sabuj Mondal <hallosauj@gmail.com>
2024-09-28 23:41:46 +0530

    Scrolling effect in the apply pesticide/fertilizer page fixed

--- front-end/src/components/apply/ApplyFertilizer.js
+++ front-end/src/components/apply/ApplyFertilizer.js
@@ -166,7 +166,7 @@ class ApplyFertilizerClass extends Component {
         {/* Section for input */}
         {this.state.plants && <div>
           <div className="flex flex-col">
-            <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="overflow-x-auto sm:-px-6 lg:-px-8">
               <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
                 <div className="overflow-x-auto">
                   <table className="min-w-full text-gray-500">
--- front-end/src/components/apply/ApplyPesticide.js
+++ front-end/src/components/apply/ApplyPesticide.js
@@ -160,7 +160,7 @@ class ApplyPesticideClass extends Component {
         {/* Section for input */}
         {this.state.plants && <div>
           <div className="flex flex-col">
-            <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="overflow-x-auto sm:-px-6 lg:-px-8">
               <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
                 <div className="overflow-x-auto">
                   <table className="min-w-full text-gray-500">
