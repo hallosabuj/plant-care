089d7d22b1f5ea8e051dab142a4be544df41cf02
Sabuj Mondal <hallosauj@gmail.com>
2024-10-23 23:18:15 +0530

    NavBar modified

--- front-end/src/components/common/NavBar.js
+++ front-end/src/components/common/NavBar.js
@@ -85,18 +85,18 @@ class NavBar2 extends Component {
 
                     {/* Middle  */}
                     <div className='hidden lg:inline'>
-                        <div className='bg-gray-800 w-auto flex items-center gap[4vh] gap-1 px-10'>
+                        <div className='bg-gray-700 md:h-11 w-auto flex items-center px-10 rounded-md'>
                             {this.state.navigation.map((item) => {
                                 return (item.public || (localStorage.getItem("isSignedIn")==='true'))&&(
                                     <NavLink
                                         key={item.name}
                                         to={item.href}
                                         className={this.state.currentPage === item.href ?
-                                            'bg-gray-700 text-white px-3 py-1 rounded-t-full rounded-b-full whitespace-nowrap' :
-                                            'hover:bg-gray-700 text-gray-400 hover:text-white px-3 py-1 rounded-t-full rounded-b-full text-sm font-medium whitespace-nowrap'
+                                            'bg-gray-500 h-full text-amber-200 px-3 whitespace-nowrap flex items-center transition-all delay-200 border-blue-50 border-b' :
+                                            'hover:bg-gray-700 hover:flex hover:items-center h-full text-gray-400 hover:text-amber-100 px-3 whitespace-nowrap pt-1 transition-colors delay-150'
                                         }
                                         onClick={() => this.handleFunc(item.href)}
-                                    > {item.name} </NavLink>
+                                    > <div>{item.name}</div> </NavLink>
                                 )
                             })}
                         </div>
@@ -141,15 +141,15 @@ class NavBar2 extends Component {
                 {/* Line 2 */}
                 <div className='flex justify-end absolute right-1'>
                     {(this.state.menuOpen) && (
-                        <div className='bg-gray-800 min-w-[200px] top-[70px] flex flex-col gap-1 px-5 pb-3 mr-4 lg:hidden rounded-b-md'>
+                        <div className='bg-gray-800 min-w-[200px] top-[70px] flex flex-col px-5 pb-3 mr-4 lg:hidden rounded-b-md'>
                             {this.state.navigation.map((item) => {
                                 return (item.public || (localStorage.getItem("isSignedIn")==='true'))&&(
                                     <NavLink
                                         key={item.name}
                                         to={item.href}
                                         className={this.state.currentPage === item.href ?
-                                            'bg-gray-700 text-white px-3 py-1 rounded-t-full rounded-b-full' :
-                                            'hover:bg-gray-700 text-gray-300 hover:text-white px-3 py-1 rounded-t-full rounded-b-full text-sm font-medium'
+                                            'bg-gray-500 text-amber-200 px-3 py-1 border-b border-blue-50' :
+                                            'hover:bg-gray-600 text-gray-300 hover:text-amber-100 px-5 hover:px-3 py-1'
                                         }
                                         onClick={() => this.handleFunc(item.href)}
                                     > {item.name} </NavLink>
@@ -165,14 +165,14 @@ class NavBar2 extends Component {
                                         key={item.name}
                                         to={item.href}
                                         className={this.state.currentPage === item.href ?
-                                            'bg-gray-700 w-fit text-white px-3 py-1 rounded-t-full rounded-b-full' :
-                                            'hover:bg-gray-700 w-fit text-gray-300 hover:text-white px-3 py-1 rounded-t-full rounded-b-full text-sm font-medium'
+                                            'bg-gray-500 w-fit text-amber-200 px-3 py-1 border-b border-blue-50' :
+                                            'hover:bg-gray-600 w-fit text-gray-300 hover:text-amber-100 px-5 hover:px-3 py-1'
                                         }
                                         onClick={() => this.handleFunc(item.href)}
                                     > {item.name} </NavLink>
                                 )
                             })}
-                            <NavLink onClick={this.signOut} to={'/'} className='hover:bg-gray-700 w-fit text-gray-300 hover:text-white px-3 py-1 rounded-t-[5%] rounded-b-[5%] rounded-l-full rounded-r-full text-sm font-medium'>Sign Out</NavLink>
+                            <NavLink onClick={this.signOut} to={'/'} className='hover:bg-gray-700 w-fit text-gray-300 hover:text-amber-100 px-3 py-1'>Sign Out</NavLink>
                         </div>
                     )}
                 </div>
