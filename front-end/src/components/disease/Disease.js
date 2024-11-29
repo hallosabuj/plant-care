b382d938120e939d0b507e8be36efc81c448280c
Sabuj Mondal <hallosauj@gmail.com>
2024-11-29 23:34:51 +0530

    Temporary changes

--- front-end/src/components/disease/Disease.js
+++ front-end/src/components/disease/Disease.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
+import './Disease.css';
 
 class Disease extends Component {
   render() {
     return (
-      <div>Disease</div>
+      <div>
+        <div className='flex justify-center items-center relative p-10'>
+          <div id="hello" className='h-24 w-24 rounded-[50%] text-2xl flex justify-center items-center' style={{background: 'conic-gradient(#00ff00 var(--fill), transparent var(--fill))', transition:'--fill 0.6s ease-in-out'}}>Hello</div>
+        </div>
+      </div>
     )
   }
 }
